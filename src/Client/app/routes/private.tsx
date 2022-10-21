a4856edbd9fa882ad7062c28439f8bc99328aa69
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-10-21 22:16:44 +0500

    express

--- src/Client/app/routes/private.tsx
+++ src/Client/app/routes/private.tsx
@@ -1,22 +1,18 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
-import type { OAuth2Profile } from "remix-auth-oauth2";
+import type { OidcProfile } from "~/utils/oidc-strategy";
 
-import { auth } from "~/utils/auth.server";
+type LoaderData = { user: OidcProfile };
 
-type LoaderData = { profile: OAuth2Profile };
+export const loader: LoaderFunction = async ({ request, context }) => {
+  const { user } = context;
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const profile = await auth.isAuthenticated(request, {
-    failureRedirect: "/",
-  });
-
-  return json<LoaderData>({ profile });
+  return json<LoaderData>({ user });
 };
 
 export default function Screen() {
-  const { profile } = useLoaderData<LoaderData>();
+  const { user } = useLoaderData<LoaderData>();
   return (
     <>
       <Form method="post" action="/logout">
@@ -26,7 +22,7 @@ export default function Screen() {
       <hr />
 
       <pre>
-        <code>{JSON.stringify(profile, null, 2)}</code>
+        <code>{JSON.stringify(user, null, 2)}</code>
       </pre>
     </>
   );
