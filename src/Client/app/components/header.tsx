f47a96319065d4faaffa42def8588e53039602c1
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-10-23 18:24:22 +0500

    add link

--- src/Client/app/components/header.tsx
+++ src/Client/app/components/header.tsx
@@ -1,4 +1,4 @@
-import { Form } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 import { useUser } from "~/utils/utils";
 
 export default function Header() {
@@ -6,7 +6,7 @@ export default function Header() {
 
   return (
     <header className="fixed w-full top-0 p-5 bg-slate-200 flex justify-between items-center">
-      {user ? user.displayName : null}
+      {user ? <Link to="/">{user.displayName}</Link> : null}
       {user ? (
         <Form method="post" action="/logout">
           <button>Log Out</button>
