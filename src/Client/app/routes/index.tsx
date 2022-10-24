0873ccfaa401316970793a58a06463f8ba7d0afa
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-10-24 09:47:50 +0500

    protect routes

--- src/Client/app/routes/index.tsx
+++ src/Client/app/routes/index.tsx
@@ -1,3 +1,10 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { auth } from "~/utils/auth.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return await auth.requireUser(request);
+};
+
 export default function Index() {
   return "Hello";
 }
--- src/Client/app/routes/private.tsx
+++ src/Client/app/routes/private.tsx
@@ -1,9 +1,17 @@
-import { useUser } from "~/utils/utils";
+import type { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { auth } from "~/utils/auth.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return await auth.requireUser(request);
+};
 
 export default function Screen() {
-  const user = useUser();
+  const user = useLoaderData();
   return (
-    <pre className="whitespace-pre-wrap break-all">{JSON.stringify(user, null, 2)}</pre>
+    <pre className="whitespace-pre-wrap break-all">
+      {JSON.stringify(user, null, 2)}
+    </pre>
   );
 }
 
