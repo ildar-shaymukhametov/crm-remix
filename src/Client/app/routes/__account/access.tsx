52f3fa059daf8071efcb3312734a9162f0418d47
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-10-27 00:04:21 +0500

    add ui for updating claims

--- src/Client/app/routes/__account/access.tsx
+++ src/Client/app/routes/__account/access.tsx
@@ -1,3 +1,112 @@
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useCatch, useLoaderData, useSubmit } from "@remix-run/react";
+import { auth } from "~/utils/auth.server";
+import Select from "react-select";
+import { useRef } from "react";
+
+type ClaimType = {
+  id: number;
+  name: string;
+  value: string;
+};
+
+type LoaderData = {
+  claimTypes: ClaimType[];
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const user = await auth.requireUser(request);
+  const formData = await request.formData();
+  const data = {
+    claims: Object.values(Object.fromEntries(formData)),
+  };
+  const response = await fetch(`${process.env.API_URL}/Users/Claims`, {
+    method: "post",
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: `Bearer ${user.extra?.accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`${response.statusText} (${response.status})`);
+  }
+
+  return null;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await auth.requireUser(request);
+  const response = await fetch(`${process.env.API_URL}/UserClaimTypes`, {
+    headers: {
+      Authorization: `Bearer ${user.extra?.accessToken}`,
+    },
+  });
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new Response("Not Found", { status: 404 });
+    }
+
+    if (response.status === 401) {
+      throw new Response("Unauthorized", { status: 401 });
+    }
+
+    return json(null, { status: response.status });
+  }
+
+  const data = await response.json();
+  return json({ claimTypes: data });
+};
+
 export default function AccessRoute() {
-  return "Access";
+  const submit = useSubmit();
+  const selectElem = useRef(null);
+  const { claimTypes } = useLoaderData<LoaderData>();
+  const options = claimTypes.map((x) => ({
+    value: x.value,
+    label: x.name,
+  }));
+
+  const onButtonClick = () => {
+    const formData = new FormData();
+    const items = selectElem.current.getValue() as [{ value: string }];
+    items.forEach((item, index) => {
+      formData.append(index.toString(), item.value);
+    });
+
+    submit(formData, { method: "post" });
+  };
+
+  return (
+    <>
+      <Select
+        ref={selectElem}
+        instanceId="user-claims"
+        isMulti
+        options={options}
+      />
+      <button type="button" onClick={onButtonClick}>
+        Save
+      </button>
+    </>
+  );
+}
+
+export function CatchBoundary() {
+  const res = useCatch();
+  if (res.status === 401) {
+    return <p>Unauthorized</p>;
+  }
+  if (res.status === 404) {
+    return <p>Company not found</p>;
+  }
+
+  throw new Error(`Unsupported thrown response status code: ${res.status}`);
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.log(error.message);
+  return <p>Unexpected error</p>;
 }
