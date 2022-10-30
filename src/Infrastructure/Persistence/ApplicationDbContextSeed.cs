6cfcb428fb692a57eaaf162ee1bd8db7cc179821
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-10-30 21:33:59 +0500

    use english

--- src/Infrastructure/Persistence/ApplicationDbContextSeed.cs
+++ src/Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -6,10 +6,10 @@ public static class ApplicationDbContextSeed
     public static void Seed(this ModelBuilder builder)
     {
         builder.Entity<UserClaimType>().HasData(
-            new UserClaimType { Id = 1, Value = "company.create", Name = "Компания. Добавление" },
-            new UserClaimType { Id = 2, Value = "company.update", Name = "Компания. Редактирование" },
-            new UserClaimType { Id = 3, Value = "company.delete", Name = "Компания. Удаление" },
-            new UserClaimType { Id = 4, Value = "company.view", Name = "Компания. Просмотр" }
+            new UserClaimType { Id = 1, Value = "company.create", Name = "Company. Create" },
+            new UserClaimType { Id = 2, Value = "company.update", Name = "Company. Update" },
+            new UserClaimType { Id = 3, Value = "company.delete", Name = "Company. Delete" },
+            new UserClaimType { Id = 4, Value = "company.view", Name = "Company. View" }
         );
     }
 }
