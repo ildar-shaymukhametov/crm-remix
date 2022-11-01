6edf8e1d44ec4828bd13b7186e4d31e7865265c8
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-11-01 19:43:41 +0500

    extract company type

---
+++ src/Client/app/models/company.ts
@@ -0,0 +1,11 @@
+export type Company = {
+  id: number;
+  type: string;
+  name: string;
+  inn: string;
+  address: string;
+  ceo: string;
+  phone: string;
+  email: string;
+  contacts: string;
+};
