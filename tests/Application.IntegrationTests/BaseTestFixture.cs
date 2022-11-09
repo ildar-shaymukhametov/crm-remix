8558c0a908560491db08c3c3c27ae50e0ac819ed
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-11-09 21:19:12 +0500

    test fixture: add replace service feature

--- tests/Application.IntegrationTests/BaseTestFixture.cs
+++ tests/Application.IntegrationTests/BaseTestFixture.cs
@@ -1,4 +1,4 @@
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using CRM.Infrastructure.Identity;
 using CRM.Infrastructure.Persistence;
 using MediatR;
@@ -23,9 +23,10 @@ public class DatabaseCollection : ICollectionFixture<BaseTestFixture>
 public class BaseTestFixture
 {
     private static WebApplicationFactory<Program> _factory = null!;
-    private static IConfiguration _configuration = null!;
+    private static WebApplicationFactory<Program>? _modifiedFactory;
+    private static WebApplicationFactory<Program> Factory => _modifiedFactory ?? _factory;
     private readonly string _connectionString;
-    private static IServiceScopeFactory _scopeFactory = null!;
+    private static IServiceScopeFactory ScopeFactory => Factory.Services.GetRequiredService<IServiceScopeFactory>();
     private static Respawner _respawner = null!;
     private static string? _currentUserId;
     public static DateTime UtcNow = Faker.Date.RandomDateTimeUtc();
@@ -34,14 +35,13 @@ public class BaseTestFixture
     public BaseTestFixture()
     {
         _factory = new CustomWebApplicationFactory();
-        _scopeFactory = _factory.Services.GetRequiredService<IServiceScopeFactory>();
-        _configuration = _factory.Services.GetRequiredService<IConfiguration>();
-        _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        var configuration = _factory.Services.GetRequiredService<IConfiguration>();
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
     }
 
     public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
     {
-        using var scope = _scopeFactory.CreateScope();
+        using var scope = ScopeFactory.CreateScope();
 
         var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
 
@@ -75,7 +75,7 @@ public class BaseTestFixture
 
     public async Task<ApplicationUser> RunAsUserAsync(string userName, string password, string[] roles, Claim[] claims)
     {
-        using var scope = _scopeFactory.CreateScope();
+        using var scope = ScopeFactory.CreateScope();
 
         var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
         var user = new ApplicationUser { UserName = userName, Email = userName };
@@ -115,6 +115,7 @@ public class BaseTestFixture
         await _respawner.ResetAsync(_connectionString);
 
         _currentUserId = null;
+        _modifiedFactory = null;
     }
 
     public async Task InitStateAsync()
@@ -127,7 +128,7 @@ public class BaseTestFixture
 
     public async Task<TEntity?> FindAsync<TEntity>(params object[] keyValues) where TEntity : class
     {
-        using var scope = _scopeFactory.CreateScope();
+        using var scope = ScopeFactory.CreateScope();
 
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
@@ -136,7 +137,7 @@ public class BaseTestFixture
 
     public async Task<List<Claim>> GetAuthorizationClaimsAsync(ApplicationUser user)
     {
-        using var scope = _scopeFactory.CreateScope();
+        using var scope = ScopeFactory.CreateScope();
         var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
         var claims = await userManager.GetClaimsAsync(user);
         return claims.Where(x => x.Type == Constants.Claims.ClaimType).ToList();
@@ -144,7 +145,7 @@ public class BaseTestFixture
 
     public async Task AddAsync<TEntity>(TEntity entity) where TEntity : class
     {
-        using var scope = _scopeFactory.CreateScope();
+        using var scope = ScopeFactory.CreateScope();
 
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
@@ -155,10 +156,26 @@ public class BaseTestFixture
 
     public async Task<int> CountAsync<TEntity>() where TEntity : class
     {
-        using var scope = _scopeFactory.CreateScope();
+        using var scope = ScopeFactory.CreateScope();
 
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         return await context.Set<TEntity>().CountAsync();
+    }
+
+    public void ReplaceServices(params (Type, object)[] replacements)
+    {
+        _modifiedFactory = Factory.WithWebHostBuilder(builder => builder.ConfigureServices(services =>
+        {
+            foreach (var (type, replacement) in replacements)
+            {
+                var serviceDescriptor = services.FirstOrDefault(d => d.ServiceType == type);
+                if (serviceDescriptor != null)
+                {
+                    services.Remove(serviceDescriptor);
+                    services.AddTransient(type, x => replacement);
+                }
+            }
+        }));
     }
 }
