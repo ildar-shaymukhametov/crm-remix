d43f8e50ed9f49110225b713a968e6757f9fdaef
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-12-06 18:13:41 +0500

    use constants

--- src/Infrastructure/Persistence/ApplicationDbContextSeed.cs
+++ src/Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -1,15 +1,18 @@
 using CRM.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using static CRM.Application.Constants;
+
+namespace CRM.Infrastructure.Persistence;
 
 public static class ApplicationDbContextSeed
 {
     public static void Seed(this ModelBuilder builder)
     {
         builder.Entity<UserClaimType>().HasData(
-            new UserClaimType { Id = 1, Value = "company.create", Name = "Company. Create" },
-            new UserClaimType { Id = 2, Value = "company.update", Name = "Company. Update" },
-            new UserClaimType { Id = 3, Value = "company.delete", Name = "Company. Delete" },
-            new UserClaimType { Id = 4, Value = "company.view", Name = "Company. View" }
+            new UserClaimType { Id = 1, Value = Claims.CreateCompany, Name = "Company. Create" },
+            new UserClaimType { Id = 2, Value = Claims.UpdateCompany, Name = "Company. Update" },
+            new UserClaimType { Id = 3, Value = Claims.DeleteCompany, Name = "Company. Delete" },
+            new UserClaimType { Id = 4, Value = Claims.ViewCompany, Name = "Company. View" }
         );
     }
 }
