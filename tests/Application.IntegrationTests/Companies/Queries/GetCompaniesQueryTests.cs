607c5ea108bf950c1be5cc92553d4f5a7a151c6e
Шаймухаметов Ильдар <shaimildar@gmail.com>
2023-05-28 23:34:42 +0500

    refactor test

--- tests/Application.IntegrationTests/Companies/Queries/GetCompaniesQueryTests.cs
+++ tests/Application.IntegrationTests/Companies/Queries/GetCompaniesQueryTests.cs
@@ -18,55 +18,43 @@ public class GetCompaniesQueryTests : BaseTest
         var request = new GetCompaniesQuery();
         var actual = await _fixture.SendAsync(request);
 
-        Assert.Collection(actual, x => Assert.True(x.Id == company.Id
-            && x.CanBeEdited
-            && x.CanBeDeleted));
+        Assert.Collection(actual, x => Assert.True(x.Id == company.Id && x.CanBeEdited && x.CanBeDeleted));
     }
 
     [Fact]
     public async Task User_can_view_any_company___Returns_companies()
     {
         var user = await _fixture.RunAsDefaultUserAsync(Constants.Claims.Company.Any.View);
-
-        var companyA = await _fixture.AddCompanyAsync();
-        var companyB = await _fixture.AddCompanyAsync();
+        var company = await _fixture.AddCompanyAsync();
 
         var request = new GetCompaniesQuery();
         var actual = await _fixture.SendAsync(request);
 
-        actual.Select(x => x.Id).Should().BeEquivalentTo(new [] { companyA.Id, companyB.Id });
+        actual.Select(x => x.Id).Should().BeEquivalentTo(new[] { company.Id });
     }
 
     [Fact]
     public async Task User_can_update_any_company___Returns_companies()
     {
         var user = await _fixture.RunAsDefaultUserAsync(Constants.Claims.Company.Any.Update);
-
-        var companyA = await _fixture.AddCompanyAsync();
-        var companyB = await _fixture.AddCompanyAsync();
+        var company = await _fixture.AddCompanyAsync();
 
         var request = new GetCompaniesQuery();
         var actual = await _fixture.SendAsync(request);
 
-        Assert.Equal(2, actual.Length);
-        Assert.Contains(actual, x => x.Id == companyA.Id && x.CanBeEdited && !x.CanBeDeleted);
-        Assert.Contains(actual, x => x.Id == companyB.Id && x.CanBeEdited && !x.CanBeDeleted);
+        Assert.Collection(actual, x => Assert.True(x.Id == company.Id && x.CanBeEdited && !x.CanBeDeleted));
     }
 
     [Fact]
     public async Task User_can_delete_any_company___Returns_companies()
     {
         var user = await _fixture.RunAsDefaultUserAsync(Constants.Claims.Company.Any.Delete);
-
-        var companyA = await _fixture.AddCompanyAsync();
-        var companyB = await _fixture.AddCompanyAsync();
+        var company = await _fixture.AddCompanyAsync();
 
         var request = new GetCompaniesQuery();
         var actual = await _fixture.SendAsync(request);
 
-        Assert.Equal(2, actual.Length);
-        Assert.Contains(actual, x => x.Id == companyB.Id && !x.CanBeEdited && x.CanBeDeleted);
-        Assert.Contains(actual, x => x.Id == companyA.Id && !x.CanBeEdited && x.CanBeDeleted);
+        Assert.Collection(actual, x => Assert.True(x.Id == company.Id && !x.CanBeEdited && x.CanBeDeleted));
     }
 
     [Fact]
@@ -84,16 +72,53 @@ public class GetCompaniesQueryTests : BaseTest
     }
 
     [Fact]
-    public async Task User_can_view_own_company___Returns_companies()
+    public async Task User_can_view_own_company_and_is_manager___Returns_companies()
     {
         var user = await _fixture.RunAsDefaultUserAsync(Constants.Claims.Company.WhereUserIsManager.View);
-
-        var companyA = await _fixture.AddCompanyAsync(user.Id);
-        var companyB = await _fixture.AddCompanyAsync();
+        var company = await _fixture.AddCompanyAsync(user.Id);
 
         var request = new GetCompaniesQuery();
         var actual = await _fixture.SendAsync(request);
 
-        Assert.Collection(actual, x => Assert.True(x.Id == companyA.Id && !x.CanBeEdited && !x.CanBeDeleted));
+        Assert.Collection(actual, x => Assert.True(x.Id == company.Id && !x.CanBeEdited && !x.CanBeDeleted));
+    }
+
+    [Theory]
+    [InlineData(Constants.Claims.Company.WhereUserIsManager.View)]
+    [InlineData(Constants.Claims.Company.WhereUserIsManager.Update)]
+    [InlineData(Constants.Claims.Company.WhereUserIsManager.Delete)]
+    public async Task User_can_view_update_delete_own_company_and_is_not_manager___Returns_empty_list(string claim)
+    {
+        await _fixture.RunAsDefaultUserAsync(new[] { claim });
+        await _fixture.AddCompanyAsync();
+
+        var request = new GetCompaniesQuery();
+        var actual = await _fixture.SendAsync(request);
+
+        Assert.Empty(actual);
+    }
+
+    [Fact]
+    public async Task User_can_update_own_company___Returns_companies()
+    {
+        var user = await _fixture.RunAsDefaultUserAsync(Constants.Claims.Company.WhereUserIsManager.Update);
+        var company = await _fixture.AddCompanyAsync(user.Id);
+
+        var request = new GetCompaniesQuery();
+        var actual = await _fixture.SendAsync(request);
+
+        Assert.Collection(actual, x => Assert.True(x.Id == company.Id && x.CanBeEdited && !x.CanBeDeleted));
+    }
+
+    [Fact]
+    public async Task User_can_delete_own_company___Returns_companies()
+    {
+        var user = await _fixture.RunAsDefaultUserAsync(Constants.Claims.Company.WhereUserIsManager.Delete);
+        var company = await _fixture.AddCompanyAsync(user.Id);
+
+        var request = new GetCompaniesQuery();
+        var actual = await _fixture.SendAsync(request);
+
+        Assert.Collection(actual, x => Assert.True(x.Id == company.Id && !x.CanBeEdited && x.CanBeDeleted));
     }
 }
